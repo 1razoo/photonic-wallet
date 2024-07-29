6604dd5bd8f9cf45f285cb42e2a716fa31228abd
photonic <photonic@localhost>
2024-07-29 10:50:41 +0000

    Use navigator.language when formatting numbers

--- packages/lib/src/format.ts
+++ packages/lib/src/format.ts
@@ -3,11 +3,11 @@ import Big from "big.js";
 export function photonsToRXD(photons: number, exact?: boolean) {
   const fixed = Big(photons).div(100000000).toString();
   return Intl.NumberFormat(
-    "en-US",
+    navigator.language,
     exact ? undefined : { maximumSignificantDigits: 12 }
   ).format(fixed as unknown as number);
 }
 
 export function formatPhotons(photons: number) {
-  return Intl.NumberFormat("en-US").format(photons);
+  return Intl.NumberFormat(navigator.language).format(photons);
 }
