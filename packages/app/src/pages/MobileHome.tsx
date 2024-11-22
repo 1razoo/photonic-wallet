18fc26dc1fbd9e6a91e7384cc5f2e1d6bfe18434
photonic <photonic@localhost>
2024-11-22 14:28:02 +0000

    Open extension in tab. Added icon to manifest.

--- packages/app/src/pages/MobileHome.tsx
+++ packages/app/src/pages/MobileHome.tsx
@@ -1,7 +1,10 @@
+import browser from "webextension-polyfill";
 import {
   Button,
   Container,
   Flex,
+  Icon,
+  IconButton,
   SimpleGrid,
   Text,
   useBreakpointValue,
@@ -14,6 +17,7 @@ import { Navigate } from "react-router-dom";
 import { network, openModal } from "@app/signals";
 import Balance from "@app/components/Balance";
 import ValueTag from "@app/components/ValueTag";
+import { TbWindowMaximize } from "react-icons/tb";
 
 export default function MobileHome() {
   const mobile = useBreakpointValue({ base: true, lg: false });
@@ -23,9 +27,37 @@ export default function MobileHome() {
     return <Navigate to="/objects" />;
   }
 
+  const isInBrowserExtensionPopup =
+    // @ts-expect-error chrome
+    ((typeof chrome !== "undefined" && chrome.runtime?.id) ||
+      (typeof browser !== "undefined" && browser.runtime?.id)) &&
+    !document.location.href.includes("?tab");
+
   return (
     <ContentContainer>
-      <PageHeader showLogo />
+      <PageHeader
+        showLogo
+        toolbar={
+          isInBrowserExtensionPopup && (
+            <IconButton
+              onClick={() => {
+                browser.tabs.create({
+                  url: "popup.html?tab",
+                });
+              }}
+              variant="ghost"
+              icon={
+                <Icon
+                  as={TbWindowMaximize}
+                  boxSize={5}
+                  transform="rotate(180deg)"
+                />
+              }
+              aria-label="Open in tab"
+            />
+          )
+        }
+      />
       <Container maxW="container.md" px={4}>
         <Card mx="auto">
           <Flex flexDirection="column" alignItems="center" mb={4}>
